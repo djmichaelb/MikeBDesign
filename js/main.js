399bb2f54db39db4c770a1d14f3b3701beeb5733
Mike Baxter <mbaxter.75@gmail.com>
2012-04-19 12:45:06 +1000

    Added graphics to the page and a lightbox

--- js/main.js
+++ js/main.js
@@ -12,17 +12,65 @@ $(function() { 	// on document ready
 		docH = $(window).height();
 		docW = $(window).width();
 		
-		$('header, #about').height(docH);
-		$('header').css('margin-top', docH);
-		window.scroll(0, docH);		
 		
-		log(docH);
-		log(docW);
+		//initialise the element sizes and margins.
+		$('header').height(docH);
+		$('header').css('margin-top', docH);
+		$('#about').css('margin-top', -($('#about').height()/2));		
+		$('#timeline').css('margin-top', (docH/4));
+		$('#timeline .headings:even').addClass('left');
+		$('#timeline .headings:odd').addClass('right');	
+		timelineH = $('#timeline').height();
+		$('#timeline:before').height(timelineH);
+		
+			
+		$('header .canvas').css('margin-top', -($('header .canvas').height()));
+		setTimeout(function() {
+			$('html, body').animate({scrollTop:	docH}, 300, 'easeInOutQuint', function() {unhideAll();});
+		},1);		
+		
+		$('header .about').on('click', function(e) {
+			e.preventDefault();
+			$('#about').scrollView('0px');
+		});
+		$('header .scroll').on('click', function(e) {
+			e.preventDefault();
+			$('#timeline').scrollView(docH*2);
+		});
+		$('#about .link').on('click', function(e) {
+			e.preventDefault();
+			$('header').scrollView(docH);
+		});
+		
+		var scrollorama = $.scrollorama({
+       	 blocks		: '.scrollable',
+       	 enablePin	: false
+    	});
+		
+		$(".fancybox").fancybox(); // attach fancybox to images
 		
 	} // #init
+	
+	function unhideAll() {
+		$('#about, #timeline').css('display', 'block');
+	} // #unhideAll
+	
+	$.fn.scrollView = function (scrollOffset) {
+	    return this.each(function () {
+	        if(!scrollOffset) {
+		        $('html, body').animate({
+		            scrollTop: $(this).offset().top
+		        }, 300, 'easeInOutQuint');
+	        } else {
+		        $('html, body').animate({
+		            scrollTop: scrollOffset
+		        }, 300, 'easeInOutQuint');
+	        }
+	    });
+	} // #scrollView function
 
 	init();
-
+	
 });
 
 // console.log -> log() function wrapper
